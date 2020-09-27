abd8c8b94c771693f67a1d9efb1aa2fadb59ca4b
AbhishekAnand <AbhishekAnand@192.168.0.105>
2020-09-27 18:23:02 +0530

    Checking into skeleton

---
+++ javaBrains-spring-security/src/main/java/spring/security/JavaBrainsSpringSecurityApplication.java
@@ -0,0 +1,17 @@
+package spring.security;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBrainsSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(JavaBrainsSpringSecurityApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+		app.run(args);
+	}
+
+}
